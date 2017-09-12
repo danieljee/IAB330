b49c54bc2caa92aaa99a0810af56088d9c81fbf9
Daniel <danieljee123@gmail.com>
2017-09-12 15:24:50 +1000

    first commit

---
+++ iab330/iab330/iab330/Views/AboutPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿
+using Xamarin.Forms;
+
+namespace iab330.Views {
+    public partial class AboutPage : ContentPage {
+        public AboutPage() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ iab330/iab330/iab330/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+using iab330.ViewModels;
+
+using Xamarin.Forms;
+
+namespace iab330.Views {
+    public partial class ItemDetailPage : ContentPage {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage() {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel) {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
