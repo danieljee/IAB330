2030e2712ddbe218e0c4db17c5ece275668ad392
David Nguyen <david.nguyen161@gmail.com>
2017-09-12 17:30:58 +1000

    Home page template

--- iab330/iab330/iab330/App.xaml.cs
+++ iab330/iab330/iab330/App.xaml.cs
@@ -13,20 +13,8 @@ namespace iab330 {
         }
 
         public static void SetMainPage() {
-            Current.MainPage = new TabbedPage {
-                Children =
-                {
-                    new NavigationPage(new ItemsPage())
-                    {
-                        Title = "Browse",
-                        Icon = Device.OnPlatform("tab_feed.png",null,null)
-                    },
-                    new NavigationPage(new AboutPage())
-                    {
-                        Title = "About",
-                        Icon = Device.OnPlatform("tab_about.png",null,null)
-                    },
-                }
+            Current.MainPage = new HomePage {
+               
             };
         }
     }
